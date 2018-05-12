3e305e339478635c1080201d31e30e7b00911480
Lucas Alderfer <lucas.alderfer@gmail.com>
2018-05-12 12:12:18 -0600

    update Key Generator to handle rotations

--- lib/key_generator.rb
+++ lib/key_generator.rb
@@ -1,17 +1,28 @@
 require 'pry'
 
 class KeyGenerator
-  attr_reader :key
-  def initialize
-    # @key = rand(00000..99999)
-    @key = []
-    generate_key
+  attr_reader :key,
+              :rotations
+  def initialize(key = [])
+    @key = key
+    @rotations = []
+    make_rotations if key.empty? == false
+    generate_key if key.empty?
   end
 
   def generate_key
-    numbers = [0,1,2,3,4,5,6,7,8,9]
+    numbers = ['0','1','2','3','4','5','6','7','8','9']
     5.times do
       @key << numbers.shuffle.pop
+    end
+    make_rotations
+  end
+
+  def make_rotations
+    index = 0
+    4.times do
+      @rotations << (@key[index] + @key[index + 1])
+      index += 1
     end
   end
 end
