8ebcb52cbe6488e1fbb8afc2bb83094df2499197
Lucas Alderfer <lucas.alderfer@gmail.com>
2018-05-14 15:30:29 -0600

    Rename Encrypt to Encryptor, Encryptor returns an encrypted message

--- test/encryptor_test.rb
+++ test/encryptor_test.rb
@@ -1,23 +1,23 @@
 require './test/test_helper.rb'
-require './lib/encrypt.rb'
+require './lib/encryptor.rb'
 require './lib/key_generator.rb'
 require './lib/offset_generator.rb'
 require './lib/character_map'
 
-class EncryptTest < Minitest::Test
+class EncryptorTest < Minitest::Test
 
   def test_it_exists
-    e = Encrypt.new
-    assert_instance_of Encrypt, e
+    e = Encryptor.new("a")
+    assert_instance_of Encryptor, e
   end
 
   def test_it_has_a_key
-    e = Encrypt.new("01234")
+    e = Encryptor.new("a","01234")
     assert_equal "01234", e.encryption_key
   end
 
   def test_it_can_combine_elements_from_key_array
-    e = Encrypt.new("01234")
+    e = Encryptor.new("a","01234")
     assert_equal 5, e.encryption_key.length
     assert_equal "01", e.rotations[0]
     assert_equal "12", e.rotations[1]
@@ -26,19 +26,19 @@ class EncryptTest < Minitest::Test
   end
 
   def test_can_group_rotations
-    e = Encrypt.new("01234")
+    e = Encryptor.new("a","01234")
     assert_equal ['01','12','23','34'], e.rotations
   end
 
   def test_can_generate_its_own_key
-   e = Encrypt.new("01234")
+   e = Encryptor.new("a","01234")
    assert_equal 4, e.rotations.length
    assert_equal 2, e.rotations[0].length
    assert_equal 2, e.rotations[3].length
   end
 
   def test_it_can_can_calculate_shift_keys
-    e = Encrypt.new("01234", Time.new(2018,05,14))
+    e = Encryptor.new("a","01234", Time.new(2018,05,14))
     assert_equal 4, e.calculate_shift_keys.length
     assert_equal 9, e.calculate_shift_keys[0]
     assert_equal 15, e.calculate_shift_keys[1]
@@ -47,8 +47,18 @@ class EncryptTest < Minitest::Test
   end
 
   def test_it_has_a_character_map
-    e = Encrypt.new
+    e = Encryptor.new("a")
     assert_equal 39, e.character_map.length
+  end
+
+  def test_it_can_encrypt_a_character
+    e = Encryptor.new("mno","01234", Time.new(2018,05,14))
+    assert_equal "v2a", e.encrypt
+  end
+
+  def test_it_can_encrypt_a_whole_message
+    e = Encryptor.new("phone booth number 1","01234", Time.new(2018,05,14))
+    assert_equal "ywamnm0nx869w9.an6w0", e.encrypt
   end
 
 end
