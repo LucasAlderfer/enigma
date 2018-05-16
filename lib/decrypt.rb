fefecfea40f23957b5fb6bfc3962ad8bbaeb5070
Lucas Alderfer <lucas.alderfer@gmail.com>
2018-05-15 18:00:37 -0600

    Complete Functionality, refactoring WIP

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,1 +1,18 @@
+require './lib/decryptor.rb'
+require 'date'
+require 'pry'
 
+content = File.open("encrypted.txt", "r")
+input = content.read.strip
+content.close
+
+content = File.new("decrypted.txt", "w")
+date = ARGV[3]
+date = date.to_s.chars
+month = date[2] + date[3]
+day = date[0] + date[1]
+year = "20" + date[4] + date[5]
+de = Decryptor.new(input, ARGV[2], Time.new(year.to_i,month.to_i,day.to_i))
+content.puts(de.decrypt)
+content.close
+puts "Created #{ARGV[1]} with the key #{de.encryption_key} and date #{ARGV[3]}"
--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -9,4 +9,5 @@ en = Encryptor.new(input)
 content = File.new("encrypted.txt", "w")
 content.puts(en.encrypt)
 content.close
-puts "Created #{ARGV[1]} with the key #{en.encryption_key.join} and date #{Date.today}"
+date = Time.now.strftime("%e%m%y").to_i
+puts "Created #{ARGV[1]} with the key #{en.encryption_key.join} and date #{date}"
